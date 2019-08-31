3bdeafb380d299d93f68f363b6c967d95882bc62
James Richters <james@productionautomation.net>
2019-08-31 14:31:01 -0400

    Set Debug to 3

--- bugtest.pas
+++ bugtest.pas
@@ -9,10 +9,11 @@ uses
 Var
   i : Integer;
 begin
+        LIBUSB_DEBUG_LEVEL:=3;
   For I:= 0 to 3 Do
      Begin
-        LIBUSB_DEBUG_LEVEL:=I;
-        Writeln('Debug Level:',I,' Looking for $10CE, $EB93: ',libusbhid_detect_device($10CE, $EB93  {WHB04B-4 CNC Handwheel},{instance=}1));
+
+        Writeln('Debug Level:',3,' Looking for $10CE, $EB93: ',libusbhid_detect_device($10CE, $EB93  {WHB04B-4 CNC Handwheel},{instance=}1));
         sleep(1500);
      End;
 end.
